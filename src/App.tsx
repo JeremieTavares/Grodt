5976488cf6f4a0e772e36d9f55f168250c98cbd6
lauriedusablon <laurie.dusablon@hotmail.com>
2025-03-23 13:21:21 -0400

    fix[completed] - updated navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Outlet } from "react-router";
-import AppSidebar from "./components/app-sidebar";
+import AppSidebar from "./components/AppSidebar";
 
 function App() {
   return (
