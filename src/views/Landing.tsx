bf7680dfc24b834a995c5fa8ac1134db75b90bbf
lauriedusablon <laurie.dusablon@hotmail.com>
2025-03-31 22:40:51 -0400

    feat [hotfix] mobile header

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -4,83 +4,81 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { ArrowRight, PieChart, Wallet, BellRing, Check } from "lucide-react";
 import GrodtBlackLogo from "../../public/medias/black-grodt-logo.svg";
 import GrodtWhiteLogo from "../../public/medias/white-grodt-logo.svg";
+import BlackLogo from "../../public/medias/black-logo.svg";
+import WhiteLogo from "../../public/medias/white-logo.svg";
 import Grodt from "../../public/medias/grodt.svg";
 import BgVideo from "../../public/medias/bg-video.mp4"
 import TwoPhones from "../../public/medias/2phones.svg";
 
+
 export default function Landing() {
   const { theme } = useTheme();
-
   return (
     <div className="min-h-screen">
-      <section>
-        <div className="relative">
-          <video
-            className="absolute w-full h-full object-cover opacity-70"
-            src={BgVideo}
-            autoPlay
-            loop
-            muted
-          ></video>
-
-          <div className="absolute inset-0 gradient-overlay"></div>
-
-          <div className="relative z-10">
-            <div className="flex items-center justify-between container mx-auto px-4 sm:px-6 lg:px-8 py-6">
-              <div className="min-[450px]:block hidden">
-                {theme === "dark" ? (
-                  <img
-                    src={GrodtWhiteLogo}
-                    height={210}
-                    width={210}
-                    alt="Grodt logo"
-                  />
-                ) : (
-                  <img
-                    src={GrodtBlackLogo}
-                    height={210}
-                    width={210}
-                    alt="Grodt logo"
-                  />
-                )}
-              </div>
-              <div className="flex gap-4 justify-end max-[450px]:justify-between w-full max-[450px]:px-4">
-                <Button
-                  size="lg"
-                  className="px-6 py-3 text-white bg-[#433BFF] hover:bg-[#3530CC] transition-all duration-200 font-medium inline-flex items-center gap-2 shadow-lg hover:shadow-[#433BFF]/25"
-                >
-                  Inscription
-                </Button>
-                <Button variant="outline" size="lg">
-                  Connexion
-                </Button>
-              </div>
-
-
-            </div>
-            <div className="text-center space-y-8 py-12 px-4 md:px-6 lg:px-8 max-w-7xl mx-auto max-[450px]:p-4">
-              <h2 className="text-4xl md:text-6xl font-bold tracking-tighter ">
-                Gérez vos dépenses avec{" "}
-                <span className="bg-gradient-to-r from-primary to-primary/65 bg-clip-text text-transparent">
-                  simplicité
-                </span>
-              </h2>
-              <p className="text-xl max-w-2xl font-medium mx-auto">
-                Prenez le contrôle de vos finances personnelles. Suivez, analysez et optimisez vos dépenses efficacement.
-              </p>
-              <div className="flex gap-4 pb-12 justify-center">
-                <Button
-                  size="lg"
-                  className="px-6 py-3 text-white bg-[#433BFF] hover:bg-[#3530CC] transition-all duration-200 font-medium inline-flex items-center gap-2 shadow-lg hover:shadow-[#433BFF]/25 group"
-                >
-                  Inscription
-                  <ArrowRight className="ml-2 h-4 w-4 transition-transform duration-300 group-hover:rotate-360" />
-                </Button>
-
-              </div>
-            </div>
+  <section>
+    <div className="relative">
+      <video
+        className="absolute w-full h-full object-cover opacity-70"
+        src={BgVideo}
+        autoPlay
+        loop
+        muted
+      ></video>
+      <div className="absolute inset-0 gradient-overlay"></div>
+      <div className="relative z-10">
+        <div className="flex items-center justify-between container mx-auto px-4 sm:px-6 lg:px-8 py-6">
+          <div className="max-[500px]:block hidden">
+            {theme === "dark" ? (
+              <img src={WhiteLogo} height={100} width={100} alt="Grodt logo" />
+            ) : (
+              <img src={BlackLogo} height={100} width={100} alt="Grodt logo" />
+            )}
+          </div>
+          <div className="hidden min-[500px]:block">
+            {theme === "dark" ? (
+              <img src={GrodtWhiteLogo} height={240} width={240} alt="Grodt logo" />
+            ) : (
+              <img src={GrodtBlackLogo} height={240} width={240} alt="Grodt logo" />
+            )}
+          </div>
+          <div className="flex gap-4 justify-end max-[450px]:justify-between w-full max-[450px]:px-4">
+            <Button
+              size="lg"
+              className="px-6 py-3 text-white bg-[#433BFF] hover:bg-[#3530CC] transition-all duration-200 font-medium inline-flex items-center gap-2 shadow-lg hover:shadow-[#433BFF]/25"
+            >
+              Inscription
+            </Button>
+            <Button variant="outline" size="lg">
+              Connexion
+            </Button>
           </div>
         </div>
+        <div className="text-center space-y-8 py-12 px-4 md:px-6 lg:px-8 max-w-7xl mx-auto max-[450px]:p-4">
+          <h2 className="text-4xl md:text-6xl font-bold tracking-tighter ">
+            Gérez vos dépenses avec{" "}
+            <span className="bg-gradient-to-r from-primary to-primary/65 bg-clip-text text-transparent">
+              simplicité
+            </span>
+          </h2>
+          <p className="text-xl max-w-2xl font-medium mx-auto">
+            Prenez le contrôle de vos finances personnelles. Suivez, analysez et optimisez vos dépenses efficacement.
+          </p>
+          <div className="flex gap-4 pb-12 justify-center">
+            <Button
+              size="lg"
+              className="px-6 py-3 text-white bg-[#433BFF] hover:bg-[#3530CC] transition-all duration-200 font-medium inline-flex items-center gap-2 shadow-lg hover:shadow-[#433BFF]/25 group"
+            >
+              Inscription
+              <ArrowRight className="ml-2 h-4 w-4 transition-transform duration-300 group-hover:rotate-360" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+        <section>
+        <div className="flex items-center justify-center flex-col lg:flex-row w-full py-12  md:px-6 lg:px-8">
         <div className="flex items-center justify-center flex-col lg:flex-row w-full py-8  md:px-6 lg:px-8">
           <div className="text-center py-6 md:py-12 max-w-7xl mx-auto">
             <h2 className="text-4xl font-bold tracking-tighter pb-4 md:pb-8">
@@ -95,8 +93,8 @@ export default function Landing() {
             <img src={Grodt} height={500} width={600} alt="Grodt logo" />
           </div>
         </div>
+        </div>
       </section>
-
       <section>
         <div className="bg-[#433bff] text-white p-6 text-center">
           <h2 className="text-2xl mb-4">
@@ -107,8 +105,6 @@ export default function Landing() {
           <h3 className="text-xl mb-2">59 000+ avis cinq étoiles</h3>
         </div>
       </section>
-
-
       <section className="pt-32 px-4 md:px-6 lg:px-8 container mx-auto">
         <div>
           <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
@@ -129,7 +125,6 @@ export default function Landing() {
                 </p>
               </CardContent>
             </Card>
-
             <Card className="hover:shadow-lg hover:shadow-[#433BFF]/25 transition-shadow duration-300 p-6">
               <CardHeader>
                 <PieChart className="h-10 w-10 text-primary mb-4" />
@@ -144,7 +139,6 @@ export default function Landing() {
                 </p>
               </CardContent>
             </Card>
-
             <Card className="hover:shadow-lg hover:shadow-[#433BFF]/25 transition-shadow duration-300 p-6">
               <CardHeader>
                 <BellRing className="h-10 w-10 text-primary mb-4" />
@@ -162,7 +156,6 @@ export default function Landing() {
           </div>
         </div>
       </section>
-
       <section>
         <div className="flex flex-col items-center justify-center px-4 md:px-10">
           <div className="px-4 py-8 md:py-30 sm:py-8 xl:pb-0 md:pb-0 w-full text-center">
@@ -184,7 +177,6 @@ export default function Landing() {
               </li>
             </ul>
           </div>
-
           <div className="pt-10 text-center">
             <Button
               size="lg"
@@ -201,8 +193,6 @@ export default function Landing() {
           alt="Image Grodt"
         />
       </section>
-
-
       <section className="relative overflow-hidden py-20 px-4 md:px-6 lg:px-8 bg-gradient-to-r from-[#433BFF] to-[#222222]">
         <div className="max-w-7xl mx-auto text-center relative z-10">
           <h2 className="text-4xl md:text-5xl font-extrabold mb-4 text-white"> Plongez dans l'expérience GRODT </h2>
@@ -221,7 +211,6 @@ export default function Landing() {
           </div>))}
         </div>
       </section>
-
       <footer className="bg-gradient-to-r from-[#433BFF] to-[#222222] text-white py-4 text-center font-light">
         <p>&copy; 2025 GRODT. Tous droits réservés</p>
       </footer>
